9ca82ab722353bdee438c317a6e64353922e8726
yongwoo97 <woo22661@naver.com>
2022-02-20 13:35:06 +0900

    못풀음 반례는 다 맞는데 왜 안되지?

---
+++ platinum/1786_찾기.py
@@ -0,0 +1,53 @@
+#kmp 알고리즘을 활용한 문자열 탐색
+#패턴에서 앞뒤가 같은 경우를 계산해서 틀린지점 부터 시작하는게 핵심 그런데 구현은 어떻게 하면 될까?
+# 그것이 문제로다
+
+
+t = input()
+p = input()
+
+def match(text, pat, start):
+    check = True
+    for i in range(start, start + len(pat)):
+        j = i - start
+
+        if text[i] == pat[j]:
+            continue
+        else:
+            check = False
+            if j <= 1:
+                return False, i + 1
+            for k in range(j-1, 1, -1):
+                if pat[:k] == pat[j - k:j]:
+                    return False, start + j - k
+
+                else:
+                    continue
+
+
+    if check:
+        j = len(pat)
+        if j <= 1:
+            return True, start + 1
+        for k in range(j-1, 1, -1):
+            if pat[:k] == pat[j - k:j]:
+                return True, start + j - k
+
+            else:
+                continue
+        return True, start + j - 1
+
+    else:
+        return False, start + len(pat)
+init = 0
+count = 0
+pos = []
+while init <= len(t) - len(p):
+    a, b = match(t, p, init)
+    if a:
+        count += 1
+        pos.append(init + 1)
+    init = b
+
+print(count)
+print(' '.join(list(map(str, pos))))
