1a99463844877e4e39085a3cc214509ddba6ac54
yongwoo97 <woo22661@naver.com>
2022-06-13 20:53:40 +0900

    AC 맞음

--- silver/1003_피보나치 함수.py
+++ silver/1003_피보나치 함수.py
@@ -4,23 +4,26 @@ n = int(input().rstrip())
 
 data = [int(input()) for _ in range(n)]
 
-
+zero_count = [1, 0]
+one_count = [0, 1]
 
 def fibo(n):
     global zero_count, one_count
-    if n != 0 and n != 1:
-        return fibo(n - 1) + fibo(n - 2)
-    if n == 0:
-        zero_count += 1
-        return 0
-    elif n == 1:
-        one_count += 1
-        return 1
+
+    if n < len(zero_count):
+        return zero_count[n], one_count[n]
+
+    for i in range(len(zero_count), n + 1):
+        zero_count.append(zero_count[i-2] + zero_count[i-1])
+        one_count.append(one_count[i-2] + one_count[i-1])
+
+    return zero_count[n], one_count[n]
+
+
+
 
 
 for i in data:
-    zero_count = 0
-    one_count = 0
-    fibo(i)
 
-    print(zero_count, one_count)
+    a, b = fibo(i)
+    print(a, b)
