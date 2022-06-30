9b0f7290b2134575f82f0a41850a0661245d9183
yongwoo97 <woo22661@naver.com>
2022-06-30 23:43:41 +0900

    mid - 1, mid 의 차이는 뭘까?

--- gold/2110_공유기설치.py
+++ gold/2110_공유기설치.py
@@ -1,5 +1,6 @@
 import sys
 input = sys.stdin.readline
+'''
 n, k = map(int, input().split())
 data = [int(input().rstrip()) for _ in range(n)]
 data.sort()
@@ -36,4 +37,39 @@ for i in range(k-2):
         result = rig
 
 print(result)
+'''
+n, c = map(int, input().split())
+
+array = []
+for i in range(n):
+    array.append(int(input()))
+
+array.sort()
+
+
+def binary_search(array, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        current = array[0]
+        count = 1
+
+        for i in range(1, len(array)):
+            if array[i] >= current + mid:
+                count += 1
+                current = array[i]
+
+        if count >= c:
+            global answer
+            start = mid + 1
+            answer = mid
+        else:
+            end = mid - 1
+
+
+start = 1
+end = array[-1] - array[0]
+answer = 0
+
+binary_search(array, start, end)
+print(answer)
 
