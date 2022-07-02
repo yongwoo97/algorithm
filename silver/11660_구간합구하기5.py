06f1fcba5bc31414a75c156f595d5b1858849e9d
yongwoo97 <woo22661@naver.com>
2022-07-02 23:39:45 +0900

    풀다 맘

--- silver/11660_구간합구하기5.py
+++ silver/11660_구간합구하기5.py
@@ -1,2 +1,10 @@
-a = [1,2]
-print(a[:0])
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+data = [list(map(int, input().split())) for _ in range(n)]
+
+su = [[0] * k for _ in range(n)]
+for i in range(n):
+    for j in range(k):
+        su[i][j] = sum(data[i][:j+1]) + sum)
