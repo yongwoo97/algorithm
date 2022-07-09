3362eed82f45032641fb03e07084bc65d00aaed9
yongwoo97 <woo22661@naver.com>
2022-07-09 19:24:57 +0900

    AC 맞음. 최소힙을 이용한 다익스트라.

--- gold/1753_최단경로.py
+++ gold/1753_최단경로.py
@@ -1,4 +1,5 @@
 #다익스트라 문제인데 어떻게 풀까?
+'''
 import sys
 input = sys.stdin.readline
 v, e = map(int, input().split())
@@ -33,5 +34,42 @@ for i in result[1:]:
 
 
 
-
+'''
 #메모리 초과로 한번 틀림
+#heapq를 이용한 다익스트라 알고리즘
+
+import sys, heapq
+input = sys.stdin.readline
+INF = sys.maxsize
+v, e = map(int, input().split())
+start = int(input())
+
+graph = [[] for _ in range(v)]
+
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    graph[a-1].append((c, b-1))
+
+q = []
+
+result = [INF] * (v)
+result[start-1] = 0
+
+heapq.heappush(q, (0, start-1))
+
+while q:
+
+    weight, next = heapq.heappop(q)
+    if result[next] < weight:
+        continue
+    for x, y in graph[next]:
+        new = x + weight
+        if result[y] > new:
+            result[y] = new
+            heapq.heappush(q, (new, y))
+
+for i in result:
+    if i == INF:
+        print('INF')
+    else:
+        print(i)
