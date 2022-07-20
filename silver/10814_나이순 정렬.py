fbc3f9b68118fe7e5db228a5fe1a05efa2471367
yongwoo97 <woo22661@naver.com>
2022-07-20 23:54:12 +0900

    퀵정렬 아직 구현 제대로 못함. 부등호 관계 공부 더 파헤치기

--- silver/10814_나이순 정렬.py
+++ silver/10814_나이순 정렬.py
@@ -49,29 +49,58 @@ print(arr)
 #퀵정렬 배열버전
 
 def quick(data):
-    print(data)
+   # print(data)
     if len(data) < 2:
         return data
     i = 0
     j = len(data) - 1
 
     mid = (i + j) // 2
-
-    while i < j:
-        print(i, j)
-        while data[i] <= data[mid] and i < len(data)-1:
+    print(data)
+    while i <= j:
+        #print(i, j)
+        while data[i] < data[mid] and i < len(data)-1:
             i += 1
         while data[j] > data[mid] and j > 0:
             j -= 1
-        print(i, j, 'hello')
-        if j <= i:
-            print(mid)
+        #print(i, j, 'hello')
+        if j < i:
+            #print(mid)
             data[mid], data[j] = data[j], data[mid]
             mid = j
-            print(mid)
-            break
-        data[i], data[j] = data[j], data[i]
-
+            #print(mid)
+        else:
+            data[i], data[j] = data[j], data[i]
+    print(data)
     return quick(data[0:mid]) + [data[mid]] + quick(data[mid+1:])
-arr = [5, 3, 2, 2, 100, 7]
+
+arr = [10, 3, 4, 5, 6]
 print(quick(arr))
+
+
+def quick_sort(array, start, end):
+    if start >= end: # 원소가 1개인 경우 종료
+        return
+    pivot = (start+end) // 2 # 피벗은 첫 번째 원소
+    left = start
+    right = end
+    while(left <= right):
+        # 피벗보다 큰 데이터를 찾을 때까지 반복
+        while(left <= end and array[left] <= array[pivot]):
+            left += 1
+        # 피벗보다 작은 데이터를 찾을 때까지 반복
+        while(right > start and array[right] >= array[pivot]):
+            right -= 1
+        #print(array)
+        if(left > right): # 엇갈렸다면 작은 데이터와 피벗을 교체
+            array[right], array[pivot] = array[pivot], array[right]
+        else: # 엇갈리지 않았다면 작은 데이터와 큰 데이터를 교체
+            array[left], array[right] = array[right], array[left]
+       # print(array)
+    #분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행
+
+    quick_sort(array, start, right - 1)
+    quick_sort(array, right + 1, end)
+
+quick_sort(arr, 0, len(arr)-1)
+print(arr)
