d1a834da2d2699a7fc420db94bf33bb77a0e5d15
yongwoo97 <woo22661@naver.com>
2022-07-30 18:54:05 +0900

    심심풀이용 문제

---
+++ 심심풀이/간단한조합문제.py
@@ -0,0 +1,17 @@
+import random
+main = round(random.random(), 5)
+a_list = []
+b_list = []
+
+a_list += [round(main-(0.1*i), 5)for i in range(1, 11)]
+b_list += [round(-(i), 5) for i in reversed(a_list)]
+
+result = a_list + b_list
+result.sort()
+result = result[len(result)//2:]
+print(main)
+for i in result:
+    for j in result:
+        if round(i+j, 5) == 0.1:
+            print(f'{i} + {j} = {round(i+j, 5)}')
+            exit()
