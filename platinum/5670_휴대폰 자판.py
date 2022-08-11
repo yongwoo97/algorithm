9d4c7baeaae9799fbfbcb6912f6082f0c3c3f338
yongwoo97 <woo22661@naver.com>
2022-08-11 22:02:36 +0900

    AC 맞음. Trie 응용

---
+++ platinum/5670_휴대폰 자판.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+
+def travel(cnt, cur):
+
+    result = 0
+    for i in cur:
+        if i == 0:
+            semi = cnt
+        elif len(cur) > 1:
+            semi = travel(cnt + 1, cur[i])
+        elif len(cur) < 2 and cnt == 0:
+            semi = travel(cnt + 1, cur[i])
+        else:
+            semi = travel(cnt, cur[i])
+        result += semi
+
+    return result
+
+
+
+
+while True:
+    n = input()
+    if n:
+        n = int(n)
+    else:
+        break
+
+    trie = {}
+    for i in range(n):
+        line = input().rstrip()
+        cur = trie
+        for j in range(len(line)):
+            if line[j] not in cur:
+                cur[line[j]] = {}
+            cur = cur[line[j]]
+        cur[0] = True
+    print(f'{travel(0, trie)/ n:.2f}')
