433cfa64543d66bd2d054fae815e62a61e33cccf
yongwoo97 <woo22661@naver.com>
2022-08-24 21:46:17 +0900

    AC 맞음.

--- silver/16139_인간-컴퓨터 상호작용.py
+++ silver/16139_인간-컴퓨터 상호작용.py
@@ -3,8 +3,14 @@ input = sys.stdin.readline
 
 str1 = input().rstrip()
 n = int(input())
-di = {chr(i):[0] * len(str1) for i in range(97, 123)}
+di = {chr(i):[0] * (len(str1)+1) for i in range(97, 123)}
 
-for i, j in enumerate(str1):
-    if i == 0:
-    di[j][i] = di[]
+for i in range(1, len(str1) + 1):
+    di[str1[i-1]][i] = di[str1[i-1]][i-1] + 1
+    for j in range(97, 123):
+        if chr(j) != str1[i-1]:
+            di[chr(j)][i] = di[chr(j)][i-1]
+
+for _ in range(n):
+    a, b, c = input().rstrip().split()
+    print(di[a][int(c)+1] - di[a][int(b)])
