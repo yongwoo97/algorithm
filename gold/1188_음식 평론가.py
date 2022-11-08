8debf16401960584ef52c946c6c2eb59d24d1e5b
yongwoo97 <woo22661@naver.com>
2022-11-08 23:52:22 +0900

    아직 못품

--- gold/1188_음식 평론가.py
+++ gold/1188_음식 평론가.py
@@ -1,5 +1,18 @@
 #좀재밌어 보이는 문제네.
 a ,b = map(int, input().split())
-semi = a / b
 
-if semi < 1:
+if a // b >= 1 and a % b == 0:
+
+    print(0)
+elif a < b:
+
+    if  b // a == 1:
+        print(1 * a)
+    else:
+        print((b//a - 1) * a)
+else:
+    re = a % b
+
+    print(re * (a-1))
+
+
