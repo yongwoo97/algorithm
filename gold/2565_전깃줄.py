48496cfbac3d7e6da936a6106494157745884955
yongwoo97 <woo22661@naver.com>
2022-11-11 23:09:04 +0900

    AC 맞음. n2 보다 빠른방법은?

---
+++ gold/2565_전깃줄.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+data = [-1] * 506
+for _ in range(n):
+    a, b = map(int, input().split())
+    data[a] = b
+
+dp = [0] * 506
+
+maxx = 0
+for i in range(501):
+    cur = data[i]
+    for j in range(i-1, -1, -1):
+        if data[j] == -1 and j != 0:
+            continue
+        if data[j] < cur:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+
+    maxx = max(maxx, dp[i])
+#print(data)
+#print(dp)
+print(n-maxx)
