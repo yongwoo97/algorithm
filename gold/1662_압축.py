b091c44d81c304ba397db698e441156bb2d4938c
yongwoo97 <woo22661@naver.com>
2022-11-23 23:23:23 +0900

    아직 못품

--- gold/1662_압축.py
+++ gold/1662_압축.py
@@ -4,33 +4,13 @@ input = sys.stdin.readline
 line = input().rstrip()
 
 from collections import deque
-def func(a):
+visited = [0] * len(line)
 
-    q = deque()
-    i = 0
-    count = ''
-    while True:
-        #print(q)
-        if q and q[-1] == ')':
-            q.pop()
-            semi = ''
-            while True:
-                x = q.pop()
-                if x == '(':
-                    x1 = int(q.pop())
-                    semi *= x1
-                    q.append(semi)
-                    break
-                else:
-                    semi += x
-        elif i < len(a):
-            q.append(a[i])
-            i += 1
-        elif not q and i >= len(a):
-            break
-        else:
-            k = q.pop()
-            count += k
-    return count
+for i in range(len(line)):
+    if line[i] == '(':
 
-print(len(func(line)))
+def func(start, end):
+    global line
+    i = start
+
+    while i < end:
