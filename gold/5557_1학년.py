b3b87bdfbc8078c373160f361acaa4adcd229095
yongwoo97 <woo22661@naver.com>
2022-12-06 22:54:13 +0900

    AC 맞음. DP접근까지는 쉽게 떠올렸는데 풀이를 완벽하게 떠올리는게 쉽지 않ㄴ네

--- gold/5557_1학년.py
+++ gold/5557_1학년.py
@@ -1,6 +1,6 @@
 n = int(input())
 data = list(map(int, input().split()))
-
+'''
 count = 0
 def func(a, i):
     global data, count, n
@@ -26,3 +26,25 @@ def func(a, i):
 
 func(0, 0)
 print(count)
+'''
+
+#이 문제는  dp로 풀어야함
+#dp의 형식을 떠올리는게 조금 어려운데... 음... 어떻게 하면될까?
+#아래와 같이 하면된다.
+
+dp = [[0] * 21 for _ in range(n-1)]
+
+
+for i in range(n-1):
+
+    if i == 0:
+        dp[i][data[i]] += 1
+        continue
+    for j in range(21):
+        if dp[i-1][j] > 0:
+            if 0 <= j + data[i] <= 20:
+                dp[i][j + data[i]] += dp[i-1][j]
+            if 0 <= j - data[i] <= 20:
+                dp[i][j - data[i]] += dp[i-1][j]
+#print(data[-1])
+print(dp[-1][data[-1]])
