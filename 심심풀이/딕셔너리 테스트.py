8ae4cc0970393ba474ba2b68b9a70150ea0f2d8f
yongwoo97 <woo22661@naver.com>
2022-12-10 23:56:10 +0900

    AC 맞음

--- 심심풀이/딕셔너리 테스트.py
+++ 심심풀이/딕셔너리 테스트.py
@@ -1,4 +1,24 @@
-d = {'b':2, 'a':1}
+from bisect import bisect_left, bisect_right
 
-print(d)
-print()
+a = [4, 9]
+x = 4
+
+print(bisect_left(a, x)) # 2
+print(bisect_right(a, x)) # 6
+#print(bisect_right(a, x) - bisect_left(a, x)) # 4
+
+
+def bina(a, n):
+    low = 0
+    high = len(a) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+        if n >= a[mid]:
+            low = mid + 1
+        else:
+            high = mid-1
+
+    return low
+
+print(bina(a, x))
