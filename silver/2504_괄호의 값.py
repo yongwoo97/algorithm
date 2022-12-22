5e3055b37b9ade009f7462e18790ef5d22da4588
yongwoo97 <woo22661@naver.com>
2022-12-22 23:36:49 +0900

    재미있는 재귀 문제네

--- silver/2504_괄호의 값.py
+++ silver/2504_괄호의 값.py
@@ -1,39 +1,36 @@
-bracket = list(input())
+bracket = input()
 
 stack = []
-answer = 0
-tmp = 1
+visited = [0] * len(bracket)
 
-for i in range(len(bracket)):
-
-    if bracket[i] == "(":
-        stack.append(bracket[i])
-        tmp *= 2
-
-    elif bracket[i] == "[":
-        stack.append(bracket[i])
-        tmp *= 3
-
-    elif bracket[i] == ")":
-        if not stack or stack[-1] == "[":
-            answer = 0
-            break
-        if bracket[i-1] == "(":
-            answer += tmp
-        stack.pop()
-        tmp //= 2
-
-    else:
-        if not stack or stack[-1] == "(":
-            answer = 0
-            break
-        if bracket[i-1] == "[":
-            answer += tmp
-
-        stack.pop()
-        tmp //= 3
-
+def func(word):
+    r = 0
+    for i in range(word, len(bracket)):
+        if visited[i] == 1:
+            continue
+        if bracket[i] == '(':
+            stack.append(bracket[i])
+            visited[i] = 1
+            r += func(i+1)
+        elif bracket[i] =='[':
+            stack.append(bracket[i])
+            visited[i] = 1
+            r += func(i+1)
+           # return r
+        elif bracket[i] == ')' and stack and stack[-1] == '(':
+            stack.pop()
+            visited[i] = 1
+            return 2 * max(1, r)
+        elif bracket[i] == ']' and stack and  stack[-1] == '[':
+            stack.pop()
+            visited[i] = 1
+            return 3 * max(1, r)
+        else:
+            print(0)
+            exit()
+    return r
+r = func(0)
 if stack:
     print(0)
 else:
-    print(answer)
+    print(r)
