b3bfd10e68f7db02d5e466a139be807a7ded7a4d
yongwoo97 <woo22661@naver.com>
2023-02-04 17:09:05 +0900

    내 풀이보다 더 좋은 방법이 있다. 아직도 갈길이 멀다

--- gold/1630_오민식.py
+++ gold/1630_오민식.py
@@ -1,16 +1,19 @@
 n = int(input())
 data = [i for i in range(1, n+1)]
-
+data = [1] + data
 summ = 1
-for i in range(n):
+for i in range(1, n+1):
     if data[i] == 1:
         continue
     summ *= data[i]
 
-    for j in range(i+1, n):
-        if data[j] == 1:
+    for j in range(2, n+1):
+        next = i * j
+        if next > n:
+            break
+        elif data[next] == 1:
             continue
-        elif data[j] % data[i] == 0:
-            data[j] //= data[i]
-
+        elif data[next] % data[i] == 0:
+            data[next] //= data[i]
+#print(data)
 print(summ % 987654321)
